0d255a93a1f83eae8cdb1ec4411b543d87cd5e3a
Marcus Liu <liu.mingku@northeastern.edu>
2023-09-20 19:30:17 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 柳铭坤 on 2023/9/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
